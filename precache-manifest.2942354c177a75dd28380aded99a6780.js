65225848785db4a458ab24476e7c6b024d8e11db
vitalcha <vkrokhov@gmail.com>
2020-07-29 15:12:53 +0300

    Updates

---
+++ precache-manifest.2942354c177a75dd28380aded99a6780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f91b289e2cf05b55518acc365c92604",
+    "url": "/Task-Yalantis/index.html"
+  },
+  {
+    "revision": "496114dd2458810e4172",
+    "url": "/Task-Yalantis/static/css/main.b783910b.chunk.css"
+  },
+  {
+    "revision": "dced13b919e66fde823c",
+    "url": "/Task-Yalantis/static/js/2.3bf1ab58.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Task-Yalantis/static/js/2.3bf1ab58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496114dd2458810e4172",
+    "url": "/Task-Yalantis/static/js/main.9fa06574.chunk.js"
+  },
+  {
+    "revision": "8b31e63b59c827b93b30",
+    "url": "/Task-Yalantis/static/js/runtime-main.b7931c5c.js"
+  }
+]);
